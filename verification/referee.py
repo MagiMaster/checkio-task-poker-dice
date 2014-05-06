57e3216f2a798254fddc3bf597e29987e5da264f
MagiMaster <MagiMaster@users.noreply.github.com>
2014-05-05 20:21:27 -0500

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -26,11 +26,54 @@ checkio.referee.cover_codes
 
 """
 
-from checkio.signals import ON_CONNECT
-from checkio import api
-from checkio.referees.io import CheckiORefereeMulti
 
 import random
+from collections import Counter
+from functools import partial
+
+def score_run(n, score, aces, die):
+    cs = Counter(die)
+    if cs.get("AS", 0) >= n:
+        return aces
+    elif max(cs.values()) >= n:
+        return score
+    else:
+        return 0
+
+def score_two_runs(n, score, aces, die):
+    cs = Counter(die).most_common()
+    if cs[0][1] >= n + 2 or (len(cs) >= 2 and cs[0][1] >= n and cs[1][1] >= 2):
+        if cs[0][0] == "AS":
+            return aces
+        else:
+            return score
+    else:
+        return 0
+
+def score_straight(die):
+    cs = Counter(die)
+    if cs == Counter(["9S", "TH", "JS", "QH", "KH"]) or cs == Counter(["TH", "JS", "QH", "KH", "AS"]):
+        return 25
+    else:
+        return 0
+
+def score_flush(die):
+    cs = Counter(die)
+    if (cs & Counter(["9S", "JS", "AS"]) == Counter()) or (cs & Counter(["TH", "QH", "KH"]) == Counter()):
+        return 15
+    else:
+        return 0
+
+score_fns = {
+    "one pair": partial(score_run, 2, 2, 3),
+    "two pair": partial(score_two_runs, 2, 4, 5),
+    "three of a kind": partial(score_run, 3, 6, 8),
+    "four of a kind": partial(score_run, 4, 50, 60),
+    "five of a kind": partial(score_run, 5, 200, 240),
+    "full house": partial(score_two_runs, 3, 25, 30),
+    "straight": score_straight,
+    "flush": score_flush
+}
 
 die = ["9S", "TH", "JS", "QH", "KH", "AS"]
 
@@ -45,23 +88,73 @@ def initial_referee(_):
         "result": True,
         "result_text": "",
         "total_score": 0,
-        "games_complete": 0,
-        "hands_complete": 0,
+        "games_completed": 0,
+        "hands_completed": 0,
         "input": [[roll(5)], {}]
     }
+
+def invalid_move(msg):
+    return {
+        "result": False,
+        "result_text": msg
+    }
+
+def next_hand(state, cat, score):
+    hands = state["hands_completed"] + 1
+    games = state["games_completed"]
+    total = state["total_score"]
+    scores = state["input"][1]
+    scores.update({cat: score})
+
+    if hands >= 8:
+        hands = 0
+        games += 1
+        total += sum(scores.values())
+        scores = {}
+        if games >= 10 and total < 1000:
+            return invalid_move("Game over. You didn't get enough points to win the tournament.")
+
+    state.update({
+        "input": [[roll(5)], scores],
+        "hands_completed": hands,
+        "games_completed": games,
+        "total_score": total
+    })
+    return state
+
+def next_roll(state, dice):
+    prev = state["input"][0]
+    scores = state["input"][1]
+    if len(prev) >= 3:
+        return invalid_move("You can only roll three times per hand.")
+
+    prev.append(dice + roll(5 - len(dice)))
+    state.update({
+        "input": [prev, scores]
+    })
+    return state
+
+def verify_dice(state, dice):
+    prev = state["input"][0][-1]
+    return Counter(prev) & Counter(dice) == Counter(dice)
 
 def process_referee(state, action):
     if isinstance(action, str):
         die = state["input"][0][-1]
         scores = state["input"][1]
-        
-
+        action = action.lower()
+        if action in score_fns.keys():
+            return next_hand(state, action, score_fns[action](die))
+        else:
+            return invalid_move("Invalid category name: " + action)
     else:
-        
-    pass
+        if not verify_dice(state, action):
+            return invalid_move("You must choose which dice to keep from the list of dice you rolled.")
+        else:
+            return next_roll(state, action)
 
 def is_win_referee(state):
-    return state["games_complete"] == 10 and state["total_score"] >= 1000
+    return state["games_completed"] == 10 and state["total_score"] >= 1000
 
 api.add_listener(
     ON_CONNECT,
